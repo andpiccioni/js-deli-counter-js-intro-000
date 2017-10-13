802208a2b6d50012d40a604f3f56f2a810ae02cc
Andrea Piccioni <andpiccioni@gmail.com>
2017-10-13 09:25:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var katzDeli = [];
 
 function takeANumber(name) {
+  var katzDeli = [];
   var katzDeliLine = katzDeli.length;
   do{
     katzDeli.push(name);
-    return `Welcome, ${name}. You are number ${katzDeliLine} in line.`;
+    return `Welcome, ${name}. You are number ${katzDeliLine + 1} in line.`;
     katzDeliLine++;
   }
   while (katzDeliLine > 0)
