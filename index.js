fd11dd07fb4cb7b4bb721a24fdbf8d54e0ad2198
Andrea Piccioni <andpiccioni@gmail.com>
2017-10-13 10:15:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,8 +16,9 @@ function takeANumber(name) {
 
 function nowServing(array){
   while (array.length > 0){
-    return `Currently serving ${array[0]}.`
-    array.shift()
+    return `Currently serving ${array[0]}.`;
+    array.shift();
+    return array
   }
     return "There is nobody waiting to be served!"
 }
